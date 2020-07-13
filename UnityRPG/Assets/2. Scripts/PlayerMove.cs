71aa85bb43e3becd3766c9416e3ba41a2f54631b
chldlwns12 <chldlwns12@gmail.com>
2020-07-13 14:03:03 +0900

    2020.07.13 -캐릭터 모델 -맵 구상 -에너미 모델

---
+++ UnityRPG/Assets/2. Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    public VariableJoystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h == 0 && v == 0)
+        {
+            h = joystick.Horizontal;
+            v = joystick.Vertical;
+        }
+
+        Vector3 dir = Vector3.right * h + Vector3.forward * v;
+        transform.Translate(dir * playerSpeed * Time.deltaTime);
+    }
+}
