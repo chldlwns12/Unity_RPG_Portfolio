b9bceaba762ef1c0b5f2367adffad70e3faaf8a7
chldlwns12 <chldlwns12@gmail.com>
2020-07-17 17:27:14 +0900

    2020.07.17 -맵 변경 및 캐릭터 변경 -캐릭터 무브 및 조이스틱 해야한다.

--- UnityRPG/Assets/2. Scripts/PlayerMove.cs
+++ UnityRPG/Assets/2. Scripts/PlayerMove.cs
@@ -2,36 +2,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMove : MonoBehaviour
 {
     public float playerSpeed = 5f;
-    public VariableJoystick joystick;
+    public Transform joystick;
 
-    // Start is called before the first frame update
-    void Start()
+    Vector3 stickFirestPos;
+    Vector3 joyVec;
+    float radius;
+    bool moveFlag;
+
+    private void Start()
     {
-        
+        radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f;
+        stickFirestPos = joystick.transform.position;
+
+        float can = transform.parent.GetComponent<RectTransform>().localScale.x;
+        radius *= can;
+
+        moveFlag = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Move();
+        if (moveFlag)
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
     }
 
-    private void Move()
+    public void Drag(BaseEventData _data)
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        if (h == 0 && v == 0)
-        {
-            h = joystick.Horizontal;
-            v = joystick.Vertical;
-        }
-
-        Vector3 dir = Vector3.right * h + Vector3.forward * v;
-        transform.Translate(dir * playerSpeed * Time.deltaTime);
+        
     }
 }
